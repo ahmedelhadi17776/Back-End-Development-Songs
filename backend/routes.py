3ddb23f08f3a9237d6e2399bfb7388f06a71f3e5
ahmedelhadi17776 <Ahmed221101032@nmu.edu.eg>
2025-09-10 03:53:50 +0300

    Add health check and count endpoint to routes

--- backend/routes.py
+++ backend/routes.py
@@ -45,9 +45,22 @@ db = client.songs
 db.songs.drop()
 db.songs.insert_many(songs_list)
 
+
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+
+
+@app.route("/health")
+def healthz():
+    return jsonify(dict(status="OK")), 200
+
+
+@app.route("/count")
+def count():
+    """return length of data"""
+    count = db.songs.count_documents({})
+    return {"count": count}, 200
